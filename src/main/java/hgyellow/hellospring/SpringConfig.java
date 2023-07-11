41b60e1e49f3827d1e1826cfb9d7b078a7d10879
jang010505 <jang010505@inu.ac.kr>
2023-07-12 06:34:58 +0900

    feat: complete 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술

---
+++ src/main/java/hgyellow/hellospring/SpringConfig.java
@@ -0,0 +1,44 @@
+package hgyellow.hellospring;
+
+import hgyellow.hellospring.aop.TimeTraceAop;
+import hgyellow.hellospring.repository.*;
+import hgyellow.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+//    private DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em){
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        // return new MemoryMemberRepository();
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcTemplateMemberRepository(dataSource);
+//        // return new JpaMemberRepository(em);
+//    }
+}
